21b5ad56d3c165a2f67fe5a1cbcd3b1b4dd6184b
Aniketh Vadthya <aniketh0701@gmail.com>
2025-04-20 13:13:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "vm_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "publisher" {
+  type = string
+}
+
+variable "offer" {
+  type = string
+}
+
+variable "sku" {
+  type = string
+}
+
+variable "version" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
